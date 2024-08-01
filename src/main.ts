811b182b18d6ee9e6e20a717da9a8228d4099ccd
JustinHodge <justinhodge8@gmail.com>
2024-07-31 22:57:44 -0400

    add assets and atlas

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,28 @@ import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
-import { Game, Types } from "phaser";
+import { Game, Types } from 'phaser';
+
+export const gameSize = {
+    width: 1024,
+    height: 768,
+    middleX: 512,
+    middleY: 384,
+};
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: gameSize.width,
+    height: gameSize.height,
     parent: 'game-container',
     backgroundColor: '#028af8',
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 export default new Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,48 @@
 import { Scene } from 'phaser';
-
-export class Game extends Scene
-{
+import { gameSize } from '../main';
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    msg_text: Phaser.GameObjects.Text;
 
-    constructor ()
-    {
+    constructor() {
         super('Game');
     }
 
-    create ()
-    {
+    create() {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(
+            gameSize.middleX,
+            gameSize.middleY,
+            'background'
+        );
         this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
+        this.anims.create({
+            key: 'walkup',
+            frames: this.anims.generateFrameNames('atlas', {
+                prefix: 'Player Character Walking Animation Frames/Zombie-Tileset---_',
+                start: 482,
+                end: 483,
+                zeroPad: 4,
+            }),
+            frameRate: 5,
+            repeat: -1,
         });
-        this.msg_text.setOrigin(0.5);
+
+        this.add
+            .sprite(
+                gameSize.middleX,
+                gameSize.height - gameSize.height / 10,
+                'atlas',
+                'Player Character Walking Animation Frames/Zombie-Tileset---_0482'
+            )
+            .play('walkup');
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('GameOver');
-
         });
     }
 }
