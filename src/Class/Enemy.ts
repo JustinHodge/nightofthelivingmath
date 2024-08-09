75139e6af21a5c26d2d9e38e098ff3d6a802e521
JustinHodge <justinhodge8@gmail.com>
2024-08-08 22:27:53 -0400

    fix instant kill

--- src/Class/Enemy.ts
+++ src/Class/Enemy.ts
@@ -86,7 +86,7 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
         this.speechBubble.y =
             this.y + this.height / 2 + this.speechBubble.height / 2;
 
-        if (this.path.length <= 0) {
+        if (this.path.length <= 0 && !this.isDead) {
             this.kill();
             this.scene.events.emit('enemyHitPlayer');
         }
