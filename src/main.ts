830e417bd28e0b895b2cea95c51bffa8f4d7227c
JustinHodge <justinhodge8@gmail.com>
2024-08-11 23:47:11 -0400

    complete main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+import {
+    GAME_BACKGROUND_COLOR,
+    GAME_DEFAULT_PHYSICS_SYSTEM,
+    GAME_HEIGHT,
+    GAME_PARENT_KEY,
+    GAME_WIDTH,
+} from './constants';
 import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
@@ -6,28 +13,21 @@ import { Preloader } from './scenes/Preloader';
 
 import { Game, Types } from 'phaser';
 
-export const gameSize = {
-    width: 1024,
-    height: 768,
-    middleX: 512,
-    middleY: 384,
-};
-
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: gameSize.width,
-    height: gameSize.height,
-    parent: 'game-container',
-    backgroundColor: '#222222',
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
+    parent: GAME_PARENT_KEY,
+    backgroundColor: GAME_BACKGROUND_COLOR,
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
     physics: {
-        default: 'arcade',
+        default: GAME_DEFAULT_PHYSICS_SYSTEM,
         arcade: {
             debug: false,
         },
